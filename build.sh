88ff592345d877a38b8a260de054c3622ddf1436
Tim Thirion <tim.thirion@kitware.com>
2017-02-27 16:46:23 -0500

    Build i386 simulator architecture too

--- build.sh
+++ build.sh
@@ -21,13 +21,14 @@ echo "create_outdir_lipo"
 for lib_x86_64 in `find $GLOBAL_OUTDIR/x86_64 -name "lib*\.a"`; do
 lib_armv7=`echo $lib_x86_64 | sed "s/x86_64/armv7/g"`
 lib_arm64=`echo $lib_x86_64 | sed "s/x86_64/arm64/g"`
+lib_i386=`echo $lib_x86_64 | sed "s/x86_64/i386/g"`
 lib=`echo $lib_x86_64 | sed "s/x86_64//g"`
 
-${LIPO} -arch armv7 $lib_armv7 -arch arm64 $lib_arm64 -arch x86_64 $lib_x86_64 -create -output $lib
+${LIPO} -arch armv7 $lib_armv7 -arch arm64 $lib_arm64 -arch i386 $lib_i386 -arch x86_64 $lib_x86_64 -create -output $lib
 done
 }
 
-mkdir -p $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/x86_64 $GLOBAL_OUTDIR/include/dcmtk
+mkdir -p $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/i386 $GLOBAL_OUTDIR/x86_64 $GLOBAL_OUTDIR/include/dcmtk
 
 cd $XCODE_DIR
 
@@ -39,11 +40,14 @@ mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/armv7/
 xcodebuild -configuration 'MinSizeRel' -sdk 'iphoneos' clean build ARCHS="arm64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$IOS_OUTDIR
 mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/arm64/
 
+xcodebuild -configuration 'MinSizeRel' -sdk 'iphonesimulator' clean build ARCHS="i386" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$IOS_OUTDIR
+mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/i386/
+
 xcodebuild -configuration 'MinSizeRel' -sdk 'iphonesimulator' clean build ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$SIM_OUTDIR
 mv $SIM_OUTDIR/* $GLOBAL_OUTDIR/x86_64/
 
 create_outdir_lipo
 
-rm -Rf $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/x86_64
+rm -Rf $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/i386 $GLOBAL_OUTDIR/x86_64
 
 cp -rf $XCODE_DIR/include/dcmtk $GLOBAL_OUTDIR/include
